92dc74608e6a483c827bc3d3c363bca3d4b61133
MOHAMED MAHER <141060515+MohamedMaherTawfik@users.noreply.github.com>
2024-03-03 12:20:20 +0200

    edit detail_view

--- ask/models.py
+++ ask/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Questions(models.Model):
     title=models.CharField(max_length=20,default='')
-    author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='question_user')
+    author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True,related_name='question_user')
     question=models.TextField(max_length=500)
     created_at=models.DateTimeField(default=timezone.now)
     draft=models.BooleanField(default=True)
@@ -17,7 +17,7 @@ class Questions(models.Model):
 class Answer(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True,related_name='User_Answer')
     answer=models.TextField(max_length=500)
-    question=models.OneToOneField(Questions,on_delete=models.SET_NULL,null=True,blank=True)
+    question=models.ForeignKey(Questions,on_delete=models.CASCADE,null=True,blank=True,related_name='answer_question')
     created_at=models.DateTimeField(default=timezone.now)
     
     def __str__(self):
